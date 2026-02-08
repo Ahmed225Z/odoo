0f3b3c6724c75832d10f2dcc57339a1baefc8c20
Ahmed225Z <100481935+Ahmed225Z@users.noreply.github.com>
2026-02-09 01:54:52 +0200

    bb

--- odoo/_monkeypatches/bs4.py
+++ odoo/_monkeypatches/bs4.py
@@ -7,3 +7,4 @@ def patch_module():
         # ofxparse use an html parser to parse ofx xml files and triggers a
         # warning since bs4 4.11.0 https://github.com/jseutter/ofxparse/issues/170
         warnings.filterwarnings('ignore', category=bs4.XMLParsedAsHTMLWarning)
+hh
